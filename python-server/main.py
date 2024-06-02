24f2a512aa4c4829dfb6374e9efcd20eac9e6fcd
Prashant Srivastav <srivastavp891@gmail.com>
2024-06-03 03:11:36 +0530

    port

--- python-server/main.py
+++ python-server/main.py
@@ -133,3 +133,7 @@ def demo():
 
 # if __name__ == '__main__':
 #     app.run(debug=True, port=8000)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
