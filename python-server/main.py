371280e7f2d754c2d3bf1ca70c6e5736ba5a1d16
Prashant Srivastav <srivastavp891@gmail.com>
2024-06-03 03:02:54 +0530

    deploy

--- python-server/main.py
+++ python-server/main.py
@@ -131,5 +131,5 @@ def demo():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+# if __name__ == '__main__':
+#     app.run(debug=True, port=8000)
